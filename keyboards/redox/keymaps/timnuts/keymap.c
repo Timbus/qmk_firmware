581d67145337ba3615eb98b1fc17f004c2dad839
Jarrod Funnell <jarrod.funnell@gmail.com>
2019-06-23 09:30:13 +1000

    Remove dumb arrow thing

--- keyboards/redox/keymaps/timnuts/keymap.c
+++ keyboards/redox/keymaps/timnuts/keymap.c
@@ -11,16 +11,17 @@ extern keymap_config_t keymap_config;
 #define _ADJUST 2
 #define _GAME 4
 
-enum custom_keycodes {
-  FARROW = SAFE_RANGE,
-  BARROW,
-  FAARROW,
-  BAARROW
-};
+// enum custom_keycodes {
+//   // FARROW = SAFE_RANGE,
+//   // BARROW,
+//   // FAARROW,
+//   // BAARROW
+// };
 
 // Shortcut to make keymap more readable
 #define SYM_L   MO(_SYMB)
 #define ADJ_L   MO(_ADJUST)
+#define FN_L    TG(_ADJUST)
 #define GAM_L   TG(_GAME)
 
 #define KC_SHRT LSFT_T(KC_ENTER)
@@ -37,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_BSPC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_6    ,                          KC_5    ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_DEL  ,
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,FN_L    ,                          GAM_L   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_DEL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_GRV  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_QUOT ,                          KC_DQUO ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_BSPC ,BARROW ,          FARROW ,KC_BSPC ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_QUES ,
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_BSPC ,KC_BSPC ,        KC_BSPC ,KC_BSPC ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_QUES ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      KC_LCTL ,KC_LGUI ,ADJ_L   ,KC_LALT ,     SYM_L   ,    KC_LCTL ,KC_CTRT ,        KC_ENTER,KC_LSFT ,    KC_SPC  ,  TT(_ADJUST),KC_LGUI ,KC_LALT ,KC_ENTER
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
@@ -51,11 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______ ,KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                             KC_EXLM, KC_AMPR, KC_PIPE, KC_QUES, KC_ASTR, KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______ ,                          _______ ,KC_TILD, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, XXXXXXX ,
+     _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, FN_L    ,                          GAM_L   ,KC_TILD, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_QUOT ,                          KC_DQUO ,KC_SLSH, KC_LPRN, KC_LBRC, KC_LCBR, KC_LT,   KC_GRAVE,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_CTLZ, KC_CTLX, KC_CTLC, KC_CTLV, KC_SPC,  _______ ,BAARROW ,        FAARROW ,_______ ,KC_BSLS, KC_RPRN, KC_RBRC, KC_RCBR, KC_GT,   KC_LGUI,
+     _______, KC_CTLZ, KC_CTLX, KC_CTLC, KC_CTLV, KC_SPC,  _______ ,KC_BSPC ,        KC_BSPC ,_______ ,KC_BSLS, KC_RPRN, KC_RBRC, KC_RCBR, KC_GT,   KC_LGUI,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      _______ ,_______ ,GAM_L   ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    KC_LCTL,      KC_LALT, KC_LGUI ,XXXXXXX ,_______
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
@@ -65,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     RESET   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+     RESET   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,FN_L    ,                          GAM_L   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -79,52 +80,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_BSPC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_T    ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_ENTER,                          GAM_L   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_DEL  ,
+     KC_TAB  ,KC_T    ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,FN_L    ,                          GAM_L   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_DEL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_GRV  ,KC_G    ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_QUOT ,                          KC_DQUO ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT ,KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    , BARROW ,         FARROW ,KC_BSPC ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_UP   ,KC_ENTER,
+     KC_LSFT ,KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    , KC_BSPC,         KC_BSPC,KC_BSPC ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_UP   ,KC_ENTER,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LCTL ,KC_LCTL ,ADJ_L   ,KC_LALT ,     KC_LCTL  ,   KC_SPC  , KC_LSFT,        KC_ENTER,KC_LSFT ,    KC_SPC  ,     KC_SLSH ,KC_LEFT ,KC_DOWN ,KC_RGHT
+     KC_LCTL ,KC_LCTL ,ADJ_L   ,KC_LALT ,     KC_LCTL ,    KC_SPC  , KC_LSFT,        KC_ENTER,KC_LSFT ,    KC_SPC  ,     KC_SLSH ,KC_LEFT ,KC_DOWN ,KC_RGHT
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case FARROW:
-      if (record->event.pressed) {
-        // when keycode is pressed
-        SEND_STRING("->");
-      } else {
-        // when keycode is released
-      }
-      break;
-    case BARROW:
-      if (record->event.pressed) {
-        // when keycode is pressed
-        SEND_STRING("<-");
-      } else {
-        // when keycode is released
-      }
-      break;
-    case FAARROW:
-      if (record->event.pressed) {
-        // when keycode is pressed
-        SEND_STRING("=>");
-      } else {
-        // when keycode is released
-      }
-      break;
-    case BAARROW:
-      if (record->event.pressed) {
-        // when keycode is pressed
-        SEND_STRING("<=");
-      } else {
-        // when keycode is released
-      }
-      break;
-  }
-  return true;
-};
+// Dumb idea for arrows that turned out lame:
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   switch (keycode) {
+//     case FARROW:
+//       if (record->event.pressed) {
+//         // when keycode is pressed
+//         SEND_STRING("->");
+//       } else {
+//         // when keycode is released
+//       }
+//       break;
+//     case BARROW:
+//       if (record->event.pressed) {
+//         // when keycode is pressed
+//         SEND_STRING("<-");
+//       } else {
+//         // when keycode is released
+//       }
+//       break;
+//     case FAARROW:
+//       if (record->event.pressed) {
+//         // when keycode is pressed
+//         SEND_STRING("=>");
+//       } else {
+//         // when keycode is released
+//       }
+//       break;
+//     case BAARROW:
+//       if (record->event.pressed) {
+//         // when keycode is pressed
+//         SEND_STRING("<=");
+//       } else {
+//         // when keycode is released
+//       }
+//       break;
+//   }
+//   return true;
+// };
